2a2c0eaf43ef4d9d14d3eb0a2bab02173dd4dc49
alexis <91147834+alexis-rudy@users.noreply.github.com>
2022-12-01 10:15:19 -0700

    finished

--- commerce/script/motions.js
+++ commerce/script/motions.js
@@ -44,4 +44,23 @@ x.onclick = toggleMenu;
 //change the button to a x when dropdown is open
 
 
+//times visited
+// initialize display elements
+const visitsDisplay = document.querySelector("#times");
+
+// get the stored value in localStorage
+let numVisits = Number(window.localStorage.getItem("visits-ls"));
+
+// determine if this is the first visit or display the number of visits.
+if (numVisits !== 0) {
+	visitsDisplay.textContent = numVisits;
+}
+
+// increment the number of visits.
+numVisits++;
+// store the new number of visits value
+localStorage.setItem("visits-ls", numVisits);
+// show todays date.
+
+
 
